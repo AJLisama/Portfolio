09ff2f2ad3e30a02f30df353b0f193f51301cd96
ducusinabdul <ducusinabdul@gmail.com>
2022-12-21 15:16:32 +0800

    Update Portfolio

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -31,8 +31,35 @@ export default function Intro() {
 						I've worked in the construction industry as a Drafter for almost 6 years. With the knowledge and skills that I aquire I'm able to use it in designing a websites.
 					</p>
 
-				</div>	
-			</div>
+				</div> {/*i-left-wrapper end*/}
+
+					<svg 
+	 					width="60px"
+	 					viewBox="0 0 14.334 24.75"
+	 					className="i-scroll"
+	 				>
+    				<circle
+    					class="circle-1"
+    					fill="black"
+    					cx="7.167"
+    					cy="6"
+    					r="1.2"
+    				/>
+    				<circle
+    					class="circle-2"
+    					fill="black"
+    					cx="7.167"
+    					cy="6"
+    					r="1.2"
+    				/>
+  					<path
+  						stroke="black"
+  						fill="transparent"
+  						d="M7.167,0.5C3.485,0.5,0.5,3.485,0.5,7.167v10.416                   c0,3.682,2.985,6.667,6.667,6.667s6.667-2.985,6.667-6.667V7.167C13.834,3.485,10.849,0.5,7.167,0.5z"
+  					/>
+					</svg>
+
+			</div> {/*i-left end*/}
 			
 			<div className="i-right">
 				<div className="dp-bg" ></div>
