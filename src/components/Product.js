37426c548afd7bbf04cd1a4d75d73e984b6acc2d
ducusinabdul <ducusinabdul@gmail.com>
2022-12-22 18:22:59 +0800

    Update Portfolio

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,19 @@
 import './product.css';
+/*import sampleProject1 from '../images/sampleProject1.PNG'*/
 
 export default function Product() {
 	return (
-		<div className="p">
+		<div className="p-card">
 			
-			<div className="p-browser">
-				
+			<div className="p-card-top">
+				<div className="p-card-circle"></div>
+				<div className="p-card-circle"></div>
+				<div className="p-card-circle"></div>
 			</div>
+
+			<a href="www.google.com" target="_blank" rel="noreferrer"> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung link*/}
+				<img className="p-img" src="" alt="" /> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung screen shot*/}
+			</a>
 
 		</div>
 	)
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,3 +1,9 @@
+import sampleProject1 from '../images/sampleProject1.PNG';
+import sampleProject2 from '../images/sampleProject2.PNG';
+import sampleProject3 from '../images/sampleProject3.png';
+import sampleProject4 from '../images/sampleProject4.png';
+import sampleProject5 from '../images/sampleProject5.png';
+import sampleProject6 from '../images/sampleProject6.png';
 import './productList.css';
 import Product from './Product.js';
 
@@ -13,12 +19,74 @@ export default function ProductList() {
 			</div>
 
 			<div className="pl-list">
-				<Product/>
-				<Product/>
-				<Product/>
-				<Product/>
-				<Product/>
-			</div>
+
+				<div className="p-card card-1">
+					<div className="p-card-top">
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+					</div>
+					<a href="google.com" target="_blank" rel="noreferrer"> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung link*/}
+						<img className="p-img" src={sampleProject1} alt="" /> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung screen shot*/}
+					</a>
+				</div> {/*p-card card-1 end*/}
+
+				<div className="p-card card-2">
+					<div className="p-card-top">
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+					</div>
+					<a href="google.com" target="_blank" rel="noreferrer"> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung link*/}
+						<img className="p-img" src={sampleProject2} alt="" /> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung screen shot*/}
+					</a>
+				</div> {/*p-card card-1 end*/}
+
+				<div className="p-card card-3">
+					<div className="p-card-top">
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+					</div>
+					<a href="google.com" target="_blank" rel="noreferrer"> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung link*/}
+						<img className="p-img" src={sampleProject3} alt="" /> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung screen shot*/}
+					</a>
+				</div> {/*p-card card-1 end*/}
+
+				<div className="p-card card-4">
+					<div className="p-card-top">
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+					</div>
+					<a href="google.com" target="_blank" rel="noreferrer"> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung link*/}
+						<img className="p-img" src={sampleProject4} alt="" /> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung screen shot*/}
+					</a>
+				</div> {/*p-card card-1 end*/}
+
+				<div className="p-card card-5">
+					<div className="p-card-top">
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+					</div>
+					<a href="google.com" target="_blank" rel="noreferrer"> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung link*/}
+						<img className="p-img" src={sampleProject5} alt="" /> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung screen shot*/}
+					</a>
+				</div> {/*p-card card-1 end*/}
+
+				<div className="p-card card-6">
+					<div className="p-card-top">
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+						<div className="p-card-circle"></div>
+					</div>
+					<a href="google.com" target="_blank" rel="noreferrer"> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung link*/}
+						<img className="p-img" src={sampleProject6} alt="" /> {/*kapag natapos ko na ang iba kong projects, ilalagay ko dito yung screen shot*/}
+					</a>
+				</div> {/*p-card card-1 end*/}
+
+			</div> {/*pl-list end*/}
 
 		</div>
 	)
